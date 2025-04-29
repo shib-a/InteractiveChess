9b24dc4370d542ef0c5ac7c6407e243b079e24fd
Мартышов Данила Викторович <409091@niuitmo.ru>
2025-04-29 23:36:18 +0300

    Started on frontend

--- frontend/src/Profile.jsx
+++ frontend/src/Profile.jsx
@@ -1,7 +1,10 @@
+import {Sidebar} from "primereact/sidebar";
+import {useState} from "react";
 const Profile = () => {
+    const [visible, setVisible] = useState(true);
   return (
       <div>
-
+            <Sidebar visible={visible}  position={"right"} onHide={() => setVisible(false)}/>
       </div>
   )
 }
--- frontend/src/WelcomePage.jsx
+++ frontend/src/WelcomePage.jsx
@@ -1,21 +1,47 @@
 import {LeaderBoard} from './LeaderBoard.jsx'
 import {Button} from "primereact/button";
-import {React} from "react";
+import React, {useEffect, useState} from "react"
 import {Profile} from "./Profile.jsx";
-const welcomePage = () => {
-    const [authKey, setAuthKey] = React.useState(null);
-    const [user, setUser] = React.useState(null);
-    const [isLoggedIn, setIsLoggedIn] = React.useState(false);
-    const handleLogin = () => {
+import axios from "axios";
+import {Dialog} from "primereact/dialog";
+import {InputText} from "primereact/inputtext";
+const WelcomePage = () => {
+    const [authKey, setAuthKey] = useState(null);
+    const [user, setUser] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [visibleLoginDialogue, setVisibleLoginDialogue] = useState(false);
+    const [loginUsername, setLoginUsername] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const handleLoginButtonClick = async () => {
+        setVisibleLoginDialogue(true);
+        await handleLoginDialogue();
+    }
+    const handleLoginDialogue = () => {
 
     }
     return (
         <div>
+            <span>cock</span>
             <div className={"profileDiv"}>
-                {isLoggedIn ? (<Button type={"button"} />) : (<Profile />)}
+                {!isLoggedIn ? (<div>
+                    <Button type={"button"} onClick={handleLoginButtonClick} label={"Login"}/>
+                    <Dialog style={{ width: '50vw' }} visible={visibleLoginDialogue} onHide={() => setVisibleLoginDialogue(false)}
+                    content={
+                        <div>
+                            <InputText value={loginUsername} onChange={(e) => setLoginUsername(e.target.value)}
+                                       defaultValue={"Login..."}/>
+                            <InputText value={loginPassword} onChange={(e) => setLoginPassword(e.target.value)}
+                                       defaultValue={"Password"}/>
+                            <Button type={"button"} onClick={(e) => setVisibleLoginDialogue(false)} label={"Log In"}/>
+                            <Button type={"button"} onClick={(e) => setVisibleLoginDialogue(false)} label={"Cancel"}/>
+                        </div>
+                    }>
+
+                    </Dialog>
+                </div>) : (<Profile/>)}
             </div>
             <div className={"leaderBoardDiv"}>
-                <LeaderBoard />
+                <LeaderBoard/>
             </div>
             <div className={"optionsDiv"}>
 
@@ -24,4 +50,4 @@ const welcomePage = () => {
     )
 }
 
-export { welcomePage };
+export {WelcomePage };
