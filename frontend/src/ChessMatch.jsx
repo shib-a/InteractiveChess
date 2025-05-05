37c8be6210cf625a5635a6462629d504e7b11280
Мартышов Данила Викторович <409091@niuitmo.ru>
2025-05-06 01:39:47 +0300

    Continued on frontend (chessboard - done chess piece movement)

--- frontend/src/ChessMatch.jsx
+++ frontend/src/ChessMatch.jsx
@@ -3,55 +3,107 @@ import React, {useEffect, useState} from "react"
 import axios from "axios";
 import {InputText} from "primereact/inputtext";
 import {BoardDataObject} from "./classes/BoardDataObject.js"
-import {figureImages} from "./classes/Figure.js";
+import {Figure, figureImages} from "./classes/Figure.js";
 
 const ChessMatch = () => {
     const height = 8;
     const width = 8;
     const currColor = "black";
-    const [chosenFigure, setChosenFigure] = useState(null);
+    const [chosenCell, setChosenCell] = useState(null);
+    const [choice, setChoice] = useState(null);
     const [boardData, setBoardData] = useState(
         BoardDataObject.populateBoards()
     );
-    const images = import.meta.glob('./resources/figures/**/*.{png,jpg,jpeg}');
-
-    const getImage = async (color, type) => {
-        const path = `./resources/figures/${color}/${color}-${type}.png`;
-        const module = await images[path](); // returns the imported image
-        return module.default;
-    };
 
     const processCellClick = (rowIndex, colIndex) =>{
-        if(boardData[rowIndex][colIndex].figure.color !== currColor && chosenFigure === null
-            || chosenFigure === null && boardData[rowIndex][colIndex].figure.type === "empty"){
+        if(boardData[rowIndex][colIndex].figure.color !== currColor && chosenCell === null
+            || chosenCell === null && boardData[rowIndex][colIndex].figure.type === "empty"){
+            console.log(boardData);
             return;
         }
         const newBoardData = boardData.map((row, rIdx) =>
             row.map((cell, cIdx) => {
-                if (rIdx === rowIndex && cIdx === colIndex) {
-                    const newCell = Object.assign(Object.create(Object.getPrototypeOf(cell)), cell);
-                    if(newCell.isHighlighted){
-                        newCell.setIsHighlighted(false);
-                        setChosenFigure(null);
-                    } else {
-                        if(chosenFigure !== null){
-                            setChosenFigure(null);
-                            newCell.setIsHighlighted(true);
-                            return newCell;
-                        }
-                        newCell.setIsHighlighted(true);
-                        setChosenFigure(newCell);
-                    }
-                    return newCell;
-                }
+                // if (rIdx === rowIndex && cIdx === colIndex) {
+                //     const newCell = Object.assign(Object.create(Object.getPrototypeOf(cell)), cell);
+                    // if(newCell.isHighlighted){
+                    //     newCell.setIsHighlighted(false);
+                    //     setChoice(null);
+                    // } else {
+                    //     if(chosenCell !== null){
+                    //         if(rIdx === rowIndex && cIdx === colIndex){
+                    //             // const replacingCell = Object.assign(Object.create(Object.getPrototypeOf(chosenFigure)), boardData[rowIndex][colIndex]);
+                    //             newCell.figure = chosenCell.figure;
+                    //             setChoice(null);
+                    //             return newCell;
+                    //         }
+                    //         if(cell.y === chosenCell.y && cell.x === chosenCell.x){
+                    //             newCell.figure.type = "empty";
+                    //             newCell.isHighlighted = false;
+                    //             console.log(cell);
+                    //             setChoice(null);
+                    //             return newCell;
+                    //         } else {
+                    //             setChoice(newCell);
+                    //             newCell.setIsHighlighted(true);
+                    //             return newCell;
+                    //         }
+                    //     }
+                    //     newCell.setIsHighlighted(true);
+                    //     setChoice(newCell);
+                    // }
+                    // return newCell;
+                // }
                 const newCell = Object.assign(Object.create(Object.getPrototypeOf(cell)), cell);
-                newCell.setIsHighlighted(false);
-                // setChosenFigure(null);
+                // newCell.setIsHighlighted(false);
+                // setChosenCell(choice);
                 return newCell;
             })
         );
+        console.log(chosenCell);
+        for(let i = 0; i < height; i++){
+            for(let j = 0; j < width; j++){
+                if (chosenCell === null) {
+                    if (i === rowIndex && j === colIndex) {
+                        if (newBoardData[i][j].isHighlighted) {
+                            newBoardData[i][j].setIsHighlighted(false);
+                            setChosenCell(null);
+                            break;
+                        }
+                        newBoardData[i][j].setIsHighlighted(true);
+                        setChosenCell(newBoardData[i][j]);
+                        break;
+                    }
+                    newBoardData[i][j].setIsHighlighted(false);
+                } else {
+                    if (i === rowIndex && j === colIndex){
+                        // console.log("here again");
+                        console.log(i,j, chosenCell.x, chosenCell.y);
+                        if (i !== chosenCell.y || j !== chosenCell.x) {
+                            newBoardData[i][j] = Object.assign(Object.create(Object.getPrototypeOf(chosenCell)), chosenCell);
+                            newBoardData[i][j].setIsHighlighted(false);
+                            newBoardData[i][j].setCoordinates(j, i);
+                            console.log(i,j);
+                            newBoardData[chosenCell.y][chosenCell.x] = new BoardDataObject(new Figure("empty"),
+                                false, chosenCell.x, chosenCell.y);
+                            console.log(newBoardData[chosenCell.x][chosenCell.y]);
+                            console.log(chosenCell);
+                            setChoice(chosenCell);
+                            setChosenCell(null);
+                            // break;
+                        } else {
+                            newBoardData[i][j].setIsHighlighted(false);
+                            setChoice(chosenCell);
+                            setChosenCell(null);
+                            // break;
+                        }
+                    }
+                }
+            }
+        }
         setBoardData(newBoardData);
-        console.log(rowIndex, colIndex);
+        // console.log(rowIndex, colIndex);
+        console.log(choice);
+        // console.log(chosenCell);
     }
     const getBackgroundColor = (rowIndex ,colIndex) => {
         let result = "";
--- frontend/src/classes/BoardDataObject.js
+++ frontend/src/classes/BoardDataObject.js
@@ -1,12 +1,21 @@
 import {Figure} from "./Figure.js";
 
 export class BoardDataObject{
-    constructor(figure, isHighlighted){
+    constructor(figure, isHighlighted, x, y){
         this.figure = figure;
         this.isHighlighted = isHighlighted
+        this.x = x;
+        this.y = y;
     }
     setIsHighlighted(isHighlighted){
         this.isHighlighted = isHighlighted
+    }
+    setFigureType(type){
+        this.figure.type = type
+    }
+    setCoordinates(x, y){
+        this.x = x;
+        this.y = y;
     }
     static populateBoards(){
     const boards = [
@@ -17,10 +26,10 @@ export class BoardDataObject{
     for (let i=0; i<height; i++) {
         for (let j=0; j<width; j++) {
             if (i < 2 || i > 5){
-                boards[i].push(new BoardDataObject(new Figure("rook", "black"), false));
+                boards[i].push(new BoardDataObject(new Figure("rook", "black"), false, j, i));
             } else {
                 // boards[i].push(new BoardDataObject(new Figure("rook", "/home/shibeyshi/IdeaProjects/InteractiveChess/frontend/src/resources/figures/white/white-rook.png"), false));
-                boards[i].push(new BoardDataObject(new Figure("empty"), false));
+                boards[i].push(new BoardDataObject(new Figure("empty"), false, j, i));
             }
         }
     }
